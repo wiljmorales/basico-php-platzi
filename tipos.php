569c65fbeacd1cde4d096e9e4d45b4261e06b753
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-03 17:45:03 -0400

    tipos

---
+++ tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+// si el string comiensa con un numero php
+// reconoce la operacion matematica
+$numerito = "8 somos";
+
+$nuevo_numerito = $numerito +2;
+
+var_dump($numerito);
+var_dump($nuevo_numerito);
+
+echo "\n";
