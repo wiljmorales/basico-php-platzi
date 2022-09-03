2c1f0d0d682cbfce4b79b17b83d7ffb8ebeb1d02
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-03 16:30:13 -0400

    debugger

---
+++ sintaxis_basica.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Holi, esto esta hecho con php \n";
+
+
+$nombre = "Wilfredo";
+
+$apellido = "Morales";
+
+// echo "Mi nombre es " . $nombre . " " . $apellido . "\n";
+
+// echo "Mi nombre es $nombre $apellido\n";
+
+// echo "tambien se sumar mira: 2 + 2 = " . (2 + 2) . "\n";
+
+$personas = [
+  "Carlos" => 22,
+  "Tommy" => 1,
+  "Mike" => 14,
+];
+
+// var_dump($personas);
+
+print_r($personas);
+
+echo "\n";
