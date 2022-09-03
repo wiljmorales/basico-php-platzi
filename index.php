ed4e776fcf6816861727a1883a7e1ea45b5d6fbd
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-03 16:17:40 -0400

    sintaxis basica

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Holi, esto esta hecho con php \n";
+
+
+$nombre = "Wilfredo";
+
+$apellido = "Morales";
+
+// echo "Mi nombre es " . $nombre . " " . $apellido . "\n";
+
+echo "Mi nombre es $nombre $apellido\n";
+
+echo "tambien se sumar mira: 2 + 2 = " . (2 + 2) . "\n";
