79eb48b6b501574b8462268b1b5732dad45cbe55
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-05 11:54:05 -0400

    Manipulacion de arrays

---
+++ manipulacion-arreglos.php
@@ -0,0 +1,21 @@
+<?php
+
+$edades = [18, 22, 40, 34];
+
+// echo count($edades);
+
+
+// array_push($edades, 13);
+
+// var_dump($edades);
+
+// var_dump(is_array("hola"));
+
+
+// combierte string en array
+// var_dump(explode(",", "pera,banana,fresa"));
+
+// combierte array en string
+var_dump(implode(",", ["fresa", "manzana", "cereza"]));
+
+echo "\n";
