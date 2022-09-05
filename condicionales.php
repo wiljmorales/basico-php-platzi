56e09e99006c52d05ab5c00f9d65a9c16bbb1353
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-05 12:18:12 -0400

    condicionales

---
+++ condicionales.php
@@ -0,0 +1,12 @@
+<?php
+
+$asientos_disponibles = 0;
+
+if ($asientos_disponibles > 0) {
+  echo "puedes ver la pelicula";
+}
+else {
+  echo "no puedes pasar esta lleno";
+}
+
+echo "\n";
---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$michi = 3;
+
+switch( $michi ) {
+  case 1: 
+    echo "Su numero favorito es el 4 \n";
+    break;
+  case 2: 
+    echo "Su numero favorito es el 7 \n";
+    break;
+  case 3: 
+    echo "Su numero favorito es el 6 \n";
+    break;
+  case 4: 
+    echo "Su numero favorito es el 1 \n";
+    break;
+}
