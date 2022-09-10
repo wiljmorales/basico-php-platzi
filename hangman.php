dd1da89ab3cad76367b5b2f6e3c99588e55737b1
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-09 21:40:07 -0400

    valores iniciales del juego

--- hangman.php
+++ hangman.php
@@ -2,9 +2,18 @@
 
 $possible_words = ["bebida", "cambur", 'mama', 'platano', 'carro'];
 
-define("MAX_ATTEMPS", 6);
+define("MAX_ATTEMPTS", 6);
 
 echo "Juego del ahorcado \n\n";
 
-$choosen_word = $possible_words[rand(0, count($possible_words))];
+$choosen_word = $possible_words[rand(0, count($possible_words) -1)];
 $word_length = strlen($choosen_word);
+$dicoveredLetters = str_pad("", ($word_length * 2), "_ ");
+
+$attempts = 0;
+
+echo "La palabra tiene $word_length palabras\n\n";
+echo ($dicoveredLetters) . "\n\n";
+
+$userLetter = readline("Escribe una letra: ");
+$userLetter = strtolower($userLetter);
