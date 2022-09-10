1201824009338bf5df6ce2372acb596d0218c9d5
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-09-10 19:45:17 -0400

    juego funcionando

--- hangman.php
+++ hangman.php
@@ -1,5 +1,13 @@
 <?php
 
+function clear() {
+  if (PHP_OS === "WINNT") {
+    system('cls');
+  } else {
+    system('clear');
+  }
+}
+
 $possible_words = ["bebida", "cambur", 'mama', 'platano', 'carro'];
 
 define("MAX_ATTEMPTS", 6);
@@ -8,12 +16,40 @@ echo "Juego del ahorcado \n\n";
 
 $choosen_word = $possible_words[rand(0, count($possible_words) -1)];
 $word_length = strlen($choosen_word);
-$dicoveredLetters = str_pad("", ($word_length * 2), "_ ");
+$dicoveredLetters = str_pad("", ($word_length ), "_");
 
 $attempts = 0;
 
-echo "La palabra tiene $word_length palabras\n\n";
-echo ($dicoveredLetters) . "\n\n";
+do {
 
-$userLetter = readline("Escribe una letra: ");
-$userLetter = strtolower($userLetter);
+  echo "La palabra tiene $word_length letras\n\n";
+  echo ($dicoveredLetters) . "\n\n";
+  
+  $userLetter = readline("Escribe una letra: ");
+  $userLetter = strtolower($userLetter);
+  
+  if (str_contains($choosen_word, $userLetter)) {
+    $offset = 0;
+    while (($letter_position = strpos($choosen_word, $userLetter, $offset)) !== false) {
+      $dicoveredLetters[$letter_position] = $userLetter;
+
+      $offset = $letter_position + 1;
+    }
+  } 
+  else {
+    clear();
+    $attempts++;
+    echo "Letra incorrecta. Te quedan ". (MAX_ATTEMPTS - $attempts) . " intentos \n";
+    sleep(2);
+  }
+  clear();
+} while ($attempts < MAX_ATTEMPTS && $dicoveredLetters != $choosen_word);
+
+clear();
+
+if($attempts < MAX_ATTEMPTS)
+  echo "Ganaste!!! \n\n";
+else
+  echo "perdiste la palabra era " . $choosen_word;
+
+echo "\n";
